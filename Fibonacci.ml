173433be8d7b6e2572ecdc563111fae3392a43cc
William Driot <william.driot@gmail.com>
2024-03-23 20:08:34 +0100

    Removed useless files and added more useful files

---
+++ Fibonacci.ml
@@ -0,0 +1,30 @@
+
+
+
+let rec fibo_nulachier n = match n with
+| 0 -> 0 
+| 1 -> 1 
+| n -> fibo_nulachier (n-1) + fibo_nulachier (n-2) ;;
+
+
+let fibo_lineaire n = 
+if n = 0 then 1 else if n = 1 then 1 else
+let a = ref 0 in 
+let b = ref 1 in 
+let u = ref 0 in 
+for k = 1 to n do 
+    u := !b ;
+    b := !a + !b ;
+    a := !u ;
+done ; n ;;
+
+
+let rec n_premiers_entiers m = 
+
+let rec aux n = match n with 
+| 1 -> [1]
+| n -> n::(aux (n-1))
+
+in List.rev (aux m) ;;
+
+List.map fibo_lineaire (n_premiers_entiers 100) ;;
