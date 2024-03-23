6335b23da7ad16e6d7f5e3a05c0cc2a4bb57c84f
William Driot <william.driot@gmail.com>
2024-03-23 20:54:24 +0100

    Added some wikipedia links to Knight's tour.ml

--- Knight's tour.ml
+++ Knight's tour.ml
@@ -1,3 +1,6 @@
 
 
 (* TODO : Create a function knight_tour : int -> int -> int that counts the number of knight tours possible (hamiltonian paths on knight graph) *)
+
+(* https://en.wikipedia.org/wiki/Knight%27s_tour
+https://en.wikipedia.org/wiki/Knight%27s_tour *)
