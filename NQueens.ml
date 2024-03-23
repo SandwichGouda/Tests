294798ff4bcee68e0eef1d6192ffa127e8f8d261
William Driot <william.driot@gmail.com>
2024-03-23 20:37:03 +0100

    Added some wikipedia links in NQueens.ml to give some possible approaches to the n queens problem

--- NQueens.ml
+++ NQueens.ml
@@ -3,3 +3,8 @@
 (* TODO : Write a nqueens n m p : int -> int -> int -> int function that returns the exact number of ways to place p queens on a n*m chessboard such that the queens don't attack eachother *)
 
 (* Leads to explore : (real) bruteforce ; backtracing ; min conflict algorithm? ; *)
+
+(* https://en.wikipedia.org/wiki/Eight_queens_puzzle 
+
+https://en.wikipedia.org/wiki/Min-conflicts_algorithm
+https://en.wikipedia.org/wiki/Eight_queens_puzzle#Exercise_in_algorithm_design *)
