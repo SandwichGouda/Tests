18c5755029dc5ae7af0b35dc1e431443d8739e35
William Driot <william.driot@gmail.com>
2024-03-23 20:33:26 +0100

    Added (* Leads to explore *) to NQueens.ml

--- NQueens.ml
+++ NQueens.ml
@@ -1,3 +1,5 @@
 
 
 (* TODO : Write a nqueens n m p : int -> int -> int -> int function that returns the exact number of ways to place p queens on a n*m chessboard such that the queens don't attack eachother *)
+
+(* Leads to explore : (real) Bruteforce ; bracktracing ; min conflict algorithm? ; *)
